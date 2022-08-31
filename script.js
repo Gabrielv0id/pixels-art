97d2ebeee71f7d137419b41cce6fcbde9be5b9b2
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-08-31 17:36:06 -0300

    exercicio 4

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 function createColors() {
   const section = document.getElementById('color-palette');
-  const firstColor = 'black';
   for (let index = 0; index < 4; index += 1) {
     const div = document.createElement('div');
     div.classList = 'color';
     section.appendChild(div);
-    if (section.firstElementChild.style.backgroundColor !== firstColor) {
-      section.firstElementChild.style.backgroundColor = firstColor;
+    if (section.firstElementChild.style.backgroundColor !== 'black') {
+      section.firstElementChild.style.backgroundColor = 'black';
+    } else if (index === 1) {
+      section.children[index].style.backgroundColor = 'red';
+    } else if (index === 2) {
+      section.children[index].style.backgroundColor = 'purple';
+    } else {
+      section.children[index].style.backgroundColor = 'blue';
+
     }
   }
-  console.log(section);
 }
 createColors();
+
+function createButton() {
+  const corpo = document.body;
+  const button = document.createElement('button');
+  button.id = 'button-random-color';
+  button.innerText = 'Cores aleatórias';
+  corpo.appendChild(button);
+}
+
+createButton();
+
+function randomColors() {
+  const div = document.querySelectorAll('.color');
+  for (let index = 1; index <= 3; index += 1) {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    div[index].style.backgroundColor = `#${randomColor}`;
+  }
+}
+
+const button = document.getElementById('button-random-color');
+button.addEventListener('click', randomColors);
